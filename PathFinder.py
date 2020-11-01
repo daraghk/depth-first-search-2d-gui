c7b2d755c7de15799aa8bb69156680183d8c47ce
Daragh King <daragh.king.2014@gmail.com>
2020-11-01 13:40:41 +0100

    updates, new SearchOnGUI class, rewrote how board is drawn,

--- PathFinder.py
+++ PathFinder.py
@@ -5,57 +5,20 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 FONT = "comicsans"
 
-class Gui:
-    def __init__(self, window_side_length, grid):
-        self.grid = grid
-        self.box_width = window_side_length / len(grid)
-        self.window_size = [window_side_length, window_side_length]
-        self.margin = 1
-        pygame.init()
-        self.screen = pygame.display.set_mode(self.window_size)
-        self.game_done = False
-        self.font = pygame.font.SysFont(FONT, 40)
-
-    def draw_original_grid(self):
-        for row in range(len(self.grid)):
-            for column in range(len(self.grid[0])):
-                pygame.draw.rect(self.screen, WHITE, self.coordinates_for_new_rectangle(row, column))
-                text = self.font.render(str(self.grid[row][column]), 1, BLACK)
-                self.screen.blit(text, self.position_to_write_text(row, column, text))
-        pygame.display.flip()
-
-    def update_grid_position_on_screen(self, row_to_change, column_to_change, update_value):
-        for row in range(len(self.grid)):
-            for column in range(len(self.grid[0])):
-                pygame.draw.rect(self.screen, WHITE, self.coordinates_for_new_rectangle(row, column))
-                if row == row_to_change and column == column_to_change:
-                    text = self.font.render(update_value, 1, BLACK)
-                    self.screen.blit(text, self.position_to_write_text(row_to_change, column_to_change, text))
-                else:
-                    text = self.font.render(str(self.grid[row][column]), 1, BLACK)
-                    self.screen.blit(text, self.position_to_write_text(row, column, text))
-        pygame.display.flip()
-
-    def coordinates_for_new_rectangle(self, row, column):
-        coordinates = [(self.margin + self.box_width) * column + self.margin,
-                       (self.margin + self.box_width) * row + self.margin,
-                       self.box_width, self.box_width]
-        return coordinates
-
-    def position_to_write_text(self, row, column, text):
-        positions = self.box_width * column - text.get_width() // 2 + self.box_width // 2, \
-                    self.box_width * row - text.get_height() // 2 + self.box_width // 2
-        return positions
+class SearchOnGUI:
+    def __init__(self, GUI):
+        self.grid = GUI.grid
+        self.GUI = GUI
 
     def depth_first_search(self, row, col, target):
         if self.grid[row][col] == target:
             self.grid[row][col] = 'Y'
-            self.update_grid_position_on_screen(row, col, 'Y')
+            self.GUI.write_underlying_grid_to_screen()
             return
 
         self.grid[row][col] = 'X'  # mark element as visited
-        self.update_grid_position_on_screen(row, col, 'X')
-        time.sleep(0.5)
+        self.GUI.write_underlying_grid_to_screen()
+        time.sleep(0.1)
 
         directions = [[0, 1], [-1, 0], [0, -1], [1, 0]]  # right, up, left, down
         for dir in directions:
@@ -73,6 +36,51 @@ class Gui:
             return False
         return True
 
+class Gui:
+    def __init__(self, window_side_length, grid):
+        self.grid = grid
+        self.BOX_WIDTH = window_side_length / len(grid)
+        self.WINDOW_SIZE = [window_side_length, window_side_length]
+        self.MARGIN = 1
+        pygame.init()
+        self.FONT = pygame.font.SysFont(FONT, 40)
+        self.SCREEN = pygame.display.set_mode(self.WINDOW_SIZE)
+        self.game_done = False
+        self.default_rectangles = self.default_rectangles()
+        self.current_rectangles = self.default_rectangles
+
+    def default_rectangles(self):
+        default_rectangles = []
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[0])):
+                rect = pygame.Rect(self.coordinates_for_new_rectangle(row, column))
+                default_rectangles.append(rect)
+        return default_rectangles
+
+    def clear_screen_text(self):
+        for rect in self.current_rectangles:
+            pygame.draw.rect(self.SCREEN, WHITE, rect)
+        pygame.display.flip()
+
+    def write_underlying_grid_to_screen(self):
+        self.clear_screen_text()
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[0])):
+                text = self.FONT.render(str(self.grid[row][column]), 1, BLACK)
+                self.SCREEN.blit(text, self.position_to_write_text(row, column, text))
+        pygame.display.flip()
+
+    def coordinates_for_new_rectangle(self, row, column):
+        coordinates = [(self.MARGIN + self.BOX_WIDTH) * column + self.MARGIN,
+                       (self.MARGIN + self.BOX_WIDTH) * row + self.MARGIN,
+                       self.BOX_WIDTH, self.BOX_WIDTH]
+        return coordinates
+
+    def position_to_write_text(self, row, column, text):
+        positions = self.BOX_WIDTH * column - text.get_width() // 2 + self.BOX_WIDTH // 2, \
+                    self.BOX_WIDTH * row - text.get_height() // 2 + self.BOX_WIDTH // 2
+        return positions
+
     def main(self):
         while not self.game_done:
             for event in pygame.event.get():
@@ -82,11 +90,16 @@ class Gui:
                     if event.key == pygame.K_SPACE:
                         self.game_done = True
                     if event.key == pygame.K_RETURN:
-                        self.draw_original_grid()
+                        self.clear_screen_text()
+                        self.write_underlying_grid_to_screen()
                     if event.key == pygame.K_s:
-                        self.depth_first_search(3, 4, 310)
+                        SearchOnGUI(self).depth_first_search(3, 4, 310)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    for rect in self.rectangles:
+                        if rect.collidepoint(pos):
+                            print(rect)
         pygame.quit()
-
 
 test_grid = [[1, 2, 3, 10, 99],
           [4, 5, 6, 3, 99],
