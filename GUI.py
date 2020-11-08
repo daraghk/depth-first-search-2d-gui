1b2a52de9a1deea625d60ba52799cfbc6290441c
Daragh King <daragh.king.2014@gmail.com>
2020-11-08 22:39:03 +0100

    remove writing of underlying grid

--- GUI.py
+++ GUI.py
@@ -1,80 +1,99 @@
 import pygame
-from Search import SearchOnGUI
+from Search import Search_On_GUI
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-FONT = "comicsans"
+ORANGE = (255, 165, 0)
+GREEN = (0, 255, 0)
 
-class Gui:
-    def __init__(self, window_side_length, grid):
-        self.grid = grid
-        self.BOX_WIDTH = window_side_length / len(grid)
-        self.WINDOW_SIZE = [window_side_length, window_side_length]
-        self.MARGIN = 1
+
+class Grid_Square:
+    def __init__(self, square: pygame.Rect, row, col):
+        self.square = square
+        self.color = WHITE
+        self.row = row
+        self.column = col
+
+
+class GUI:
+    def __init__(self, screen_width, grid):
         pygame.init()
-        self.FONT = pygame.font.SysFont(FONT, 40)
-        self.SCREEN = pygame.display.set_mode(self.WINDOW_SIZE)
+        self.grid = grid
+        self.BOX_WIDTH = screen_width / len(grid)
+        self.MARGIN = 1
+        self.SCREEN = pygame.display.set_mode([screen_width, screen_width])
+
         self.game_done = False
-        self.default_rectangles = self.default_rectangles()
-        self.current_rectangles = self.default_rectangles
+        self.default_squares = self.create_default_squares()
+        self.current_squares = self.default_squares
+        self.current_squares_map = {
+            grid_square: grid_square for grid_square in self.current_squares}
 
-    def default_rectangles(self):
-        default_rectangles = []
+    def create_default_squares(self):
+        default_squares = []
         for row in range(len(self.grid)):
             for column in range(len(self.grid[0])):
-                rect = pygame.Rect(self.coordinates_for_new_rectangle(row, column))
-                default_rectangles.append(rect)
-        return default_rectangles
+                square = Grid_Square(
+                    pygame.Rect(
+                        self.coordinates_for_new_square(row, column)),
+                    row,
+                    column)
+                default_squares.append(square)
+        return default_squares
 
-    def clear_screen_text(self):
-        for rect in self.current_rectangles:
-            pygame.draw.rect(self.SCREEN, WHITE, rect)
+    def update_square_color(self, square_to_update: Grid_Square, color):
+        self.current_squares_map[square_to_update].color = color
+        pygame.draw.rect(self.SCREEN, color, square_to_update.square)
         pygame.display.flip()
 
-    def write_underlying_grid_to_screen(self):
-        self.clear_screen_text()
-        for row in range(len(self.grid)):
-            for column in range(len(self.grid[0])):
-                text = self.FONT.render(str(self.grid[row][column]), 1, BLACK)
-                self.SCREEN.blit(text, self.position_to_write_text(row, column, text))
+    def draw_squares(self):
+        for grid_square in self.current_squares:
+            pygame.draw.rect(self.SCREEN, grid_square.color,
+                             grid_square.square)
         pygame.display.flip()
 
-    def coordinates_for_new_rectangle(self, row, column):
+    def coordinates_for_new_square(self, row, column):
         coordinates = [(self.MARGIN + self.BOX_WIDTH) * column + self.MARGIN,
                        (self.MARGIN + self.BOX_WIDTH) * row + self.MARGIN,
                        self.BOX_WIDTH, self.BOX_WIDTH]
         return coordinates
 
-    def position_to_write_text(self, row, column, text):
-        positions = self.BOX_WIDTH * column - text.get_width() // 2 + self.BOX_WIDTH // 2, \
-                    self.BOX_WIDTH * row - text.get_height() // 2 + self.BOX_WIDTH // 2
-        return positions
-
     def main(self):
+        left_click_occurred = False
+        right_click_happened = False
+        start_position = []
+        end_position = []
+        self.draw_squares()
+        
         while not self.game_done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.game_done = True
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         self.game_done = True
-                    if event.key == pygame.K_RETURN:
-                        self.clear_screen_text()
-                        self.write_underlying_grid_to_screen()
                     if event.key == pygame.K_s:
-                        SearchOnGUI(self).depth_first_search(3, 4, 310)
+                        if left_click_occurred and right_click_happened:
+                            Search_On_GUI(self).depth_first_search(start_position[0], start_position[1],
+                                                                   end_position[0], end_position[1])
+
                 if event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
-                    for rect in self.current_rectangles:
-                        if rect.collidepoint(pos):
-                            print(rect)
+                    for grid_square in self.current_squares:
+                        if grid_square.square.collidepoint(pos):
+                            if event.button == 1 and not left_click_occurred:
+                                self.update_square_color(grid_square, GREEN)
+                                start_position = [
+                                    grid_square.row, grid_square.column]
+                                left_click_occurred = True
+                            elif event.button == 3 and not right_click_happened:
+                                self.update_square_color(grid_square, ORANGE)
+                                end_position = [
+                                    grid_square.row, grid_square.column]
+                                right_click_happened = True
         pygame.quit()
 
-test_grid = [[1, 2, 3, 10, 99],
-          [4, 5, 6, 3, 99],
-          [5, 34, 6, 7, 99],
-          [56, 67, 34, 90, 99],
-          [100, 34, 67, 310, 99]]
-
-gui = Gui(600, test_grid)
+test_grid = [[None]*8 for i in range(8)]
+gui = GUI(600, test_grid)
 gui.main()
--- Search.py
+++ Search.py
@@ -1,26 +1,33 @@
 import time
 
-class SearchOnGUI:
+RED = (255, 0, 0)
+
+class Search_On_GUI:
     def __init__(self, GUI):
         self.grid = GUI.grid
         self.GUI = GUI
-        self.directions = [[0, 1], [-1, 0], [0, -1], [1, 0]]  # right, up, left, down
+        self.directions = [[0, 1], [1,0], [-1,0], [0, -1]]  # right, down, left, up
+        self.goal_found = False
 
-    def depth_first_search(self, row, col, target):
-        if self.grid[row][col] == target:
-            self.grid[row][col] = 'Y' # Found
-            self.GUI.write_underlying_grid_to_screen()
-            return
+    def depth_first_search(self, row, col, target_row, target_col):
+        if not self.goal_found:
+            if row == target_row and col == target_col:
+                self.grid[row][col] = 'Y'  # Found
+                self.goal_found = True
+                return
 
-        self.grid[row][col] = 'X'  # Visited
-        self.GUI.write_underlying_grid_to_screen()
-        time.sleep(0.1)
+            self.grid[row][col] = 'X'  # Visited
+            for square in self.GUI.current_squares:
+                if square.row == row and square.column == col:
+                    self.GUI.update_square_color(square, (255, 0, 0))
+            time.sleep(0.075)
 
-        for direction in self.directions:
-            new_row = row + direction[0]
-            new_col = col + direction[1]
-            if self.is_move_valid(new_row, new_col):
-                return self.depth_first_search(new_row, new_col, target)
+            for direction in self.directions:
+                new_row = row + direction[0]
+                new_col = col + direction[1]
+                if self.is_move_valid(new_row, new_col):
+                    self.depth_first_search(
+                        new_row, new_col, target_row, target_col)
 
     def is_move_valid(self, row, col):
         if row < 0 or row > len(self.grid) - 1:
